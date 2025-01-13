d77b4c835b5729ab3ce8375670ffe32dff0521bc
DasunNirmal <dasun200301@gmail.com>
2025-01-13 22:43:52 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {createBrowserRouter, RouterProvider} from "react-router";
 import {RootLayout} from "./components/RootLayout.tsx";
 import {Dashboard} from "./pages/Dashboard.tsx";
 import {SignIn} from "./pages/SignIn.tsx";
+import {SignUp} from "./pages/SignUp.tsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
            element : <RootLayout/>,
            children:[
                {path : '',element : <SignIn/>},
+               {path : '/signup',element : <SignUp/>},
                {path : '/home',element : <Dashboard/>}
            ]},
   ])
