8a037fcac6b2fa86491db93520452fa9da3db4f1
DasunNirmal <dasun200301@gmail.com>
2025-01-13 12:33:53 +0530

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import {RootLayout} from "./components/RootLayout.tsx";
 import {Dashboard} from "./pages/Dashboard.tsx";
+import {SignIn} from "./pages/SignIn.tsx";
 
 function App() {
 
-   const routers =  createBrowserRouter([
-        {path : '',
-            element : <RootLayout/>,
-            children:[
-                {path : '/home',element : <Dashboard/>}
-            ]},
-   ])
+  const routers =  createBrowserRouter([
+       {path : '',
+           element : <RootLayout/>,
+           children:[
+               {path : '',element : <SignIn/>},
+               {path : '/home',element : <Dashboard/>}
+           ]},
+  ])
 
   return (
       <>
