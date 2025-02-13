210ee571a47c7245ab7405acf4985fc9d9b40f37
DasunNirmal <dasun200301@gmail.com>
2025-02-13 15:14:17 +0530

    small changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,13 @@ import 'animate.css';
 import './normalize.css';
 import './index.css'
 import App from './App.tsx'
+import {Provider} from "react-redux";
+import {store} from "./store/Store.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </StrictMode>,
 )
