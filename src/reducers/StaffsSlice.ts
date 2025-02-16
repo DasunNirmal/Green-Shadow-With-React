45550900f935801aca2bc2a683261d45d2b25e19
DasunNirmal <dasun200301@gmail.com>
2025-02-16 14:01:42 +0530

    done

--- src/reducers/StaffsSlice.ts
+++ src/reducers/StaffsSlice.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Staffs from "../models/Staffs.ts";
-import {createAsyncThunk} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
 export const initialState : Staffs[] = [];
 
@@ -67,3 +67,81 @@ export const searchStaffs = createAsyncThunk(
         }
     }
 );
+
+const StaffsSlice = createSlice({
+    name: 'staffs',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(saveStaffs.fulfilled, (state, action) => {
+                state.push(action.payload);
+            })
+            .addCase(saveStaffs.rejected, (state, action) => {
+                console.error(action.error.message);
+            })
+            .addCase(saveStaffs.pending, (state, action) => {
+                console.log('Pending saving staff : ', action.payload);
+            });
+        builder
+            .addCase(deleteStaffs.fulfilled, (state, action) => {
+                return state.filter(staff => staff.staff_id !== action.payload);
+            })
+            .addCase(deleteStaffs.rejected, (state, action) => {
+                console.error('Error deleting staff : ',action.payload);
+            })
+            .addCase(deleteStaffs.pending, (state, action) => {
+                console.log('Pending deleting staff : ', action.payload);
+            });
+        builder
+            .addCase(updateStaffs.fulfilled, (state, action) => {
+                state.map((staffs) => {
+                    if (staffs.staff_id === action.payload.staff_id) {
+                        staffs.staff_id = action.payload.staff_id;
+                        staffs.address_01 = action.payload.address_01;
+                        staffs.address_02 = action.payload.address_02;
+                        staffs.address_03 = action.payload.address_03;
+                        staffs.address_04 = action.payload.address_04;
+                        staffs.address_05 = action.payload.address_05;
+                        staffs.designation = action.payload.designation;
+                        staffs.dob = action.payload.dob;
+                        staffs.email = action.payload.email;
+                        staffs.first_name = action.payload.first_name;
+                        staffs.gender = action.payload.gender;
+                        staffs.joined_date = action.payload.joined_date;
+                        staffs.last_name = action.payload.last_name;
+                        staffs.phone_no = action.payload.phone_no;
+                        staffs.role = action.payload.role;
+                    }
+                });
+            })
+            .addCase(updateStaffs.rejected, (state, action) => {
+                console.error(action.error.message);
+            })
+            .addCase(updateStaffs.pending, (state, action) => {
+                console.log('Pending updating staff : ', action.payload);
+            });
+        builder
+            .addCase(getStaffs.fulfilled, (state, action) => {
+                return Array.isArray(action.payload) ? action.payload : [];
+            })
+            .addCase(getStaffs.rejected, (state, action) => {
+                console.error(action.error.message);
+            })
+            .addCase(getStaffs.pending, (state, action) => {
+                console.log('Pending getting staff : ', action.payload);
+            });
+        builder
+            .addCase(searchStaffs.fulfilled, (state, action) => {
+                return Array.isArray(action.payload) ? action.payload : [];
+            })
+            .addCase(searchStaffs.rejected, (state, action) => {
+                console.error(action.error.message);
+            })
+            .addCase(searchStaffs.pending, (state, action) => {
+                console.log('Pending searching staff : ', action.payload);
+            });
+    }
+});
+
+export default StaffsSlice.reducer;
