6ee2a9d8b261f6e3616e8c2b8bc9ea80f2f22cb7
DasunNirmal <dasun200301@gmail.com>
2025-02-16 13:56:32 +0530

    class for staff

---
+++ src/models/Staffs.ts
@@ -0,0 +1,17 @@
+export default class Staffs {
+    staff_id!:string;
+    address_01!:string;
+    address_02!:string;
+    address_03!:string;
+    address_04!:string;
+    address_05!:string;
+    designation!:string;
+    dob!:string;
+    email!:string;
+    first_name!:string;
+    gender!:string;
+    joined_date!:string;
+    last_name!:string;
+    phone_no!:string;
+    role!:string;
+}
