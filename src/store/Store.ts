64936ed280f38f76db1b487b85ed6d2c1f85f2c2
DasunNirmal <dasun200301@gmail.com>
2025-02-18 20:43:19 +0530

    Equipment added

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -3,13 +3,15 @@ import FieldsSlice from "../reducers/FieldsSlice.ts";
 import CropsSlice from "../reducers/CropsSlice.ts";
 import StaffsSlice from "../reducers/StaffsSlice.ts";
 import VehicleSlice from "../reducers/VehicleSlice.ts";
+import EquipmentSlice from "../reducers/EquipmentSlice.ts";
 
 export const store = configureStore({
    reducer: {
        fields: FieldsSlice,
        crops: CropsSlice,
        staffs: StaffsSlice,
-       vehicles: VehicleSlice
+       vehicles: VehicleSlice,
+       equipments: EquipmentSlice
    }
 });
 
