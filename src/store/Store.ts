6fa685ca3c9361969ea8945bf086cfd2938beceb
DasunNirmal <dasun200301@gmail.com>
2025-02-18 10:45:52 +0530

    Vehicle

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import FieldsSlice from "../reducers/FieldsSlice.ts";
 import CropsSlice from "../reducers/CropsSlice.ts";
 import StaffsSlice from "../reducers/StaffsSlice.ts";
+import VehicleSlice from "../reducers/VehicleSlice.ts";
 
 export const store = configureStore({
    reducer: {
        fields: FieldsSlice,
        crops: CropsSlice,
-       staffs: StaffsSlice
+       staffs: StaffsSlice,
+       vehicles: VehicleSlice
    }
 });
 
