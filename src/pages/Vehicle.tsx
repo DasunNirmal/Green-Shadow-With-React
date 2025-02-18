35d3551452673c6bc08f0091a013f292af3281ba
DasunNirmal <dasun200301@gmail.com>
2025-02-18 10:46:14 +0530

    search staff done

--- src/pages/Vehicle.tsx
+++ src/pages/Vehicle.tsx
@@ -1,7 +1,47 @@
 import './Vehicle.css'
 import './formControll.css'
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "../store/Store.ts";
+import Vehicles from "../models/Vehicle.ts";
+import {useState} from "react";
+import {searchStaffs} from "../reducers/StaffsSlice.ts";
 
 export const Vehicle = () => {
+
+    const dispatch = useDispatch<AppDispatch>();
+    const vehicles = useSelector((state: { vehicles: Vehicles[] }) => state.vehicles);
+
+    const [vehicleCode, setVehicleCode] = useState('');
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [fuelType, setFuelType] = useState('');
+    const [licensePlate, setLicensePlate] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [remarks, setRemarks] = useState('');
+    const [role, setRole] = useState('');
+    const [status, setStatus] = useState('');
+    const [vehicleCategory, setVehicleCategory] = useState('');
+    const [staffID, setStaffID] = useState('');
+    const [SearchedStaff, setSearchedStaff] = useState('');
+    const [SearchedVehicle, setSearchedVehicle] = useState('');
+
+    const handleStaffSearch = async () => {
+        try {
+            const fetchedStaffs = await dispatch(searchStaffs(SearchedStaff));
+            if (fetchedStaffs.payload) {
+                setStaffID(fetchedStaffs.payload.staff_id);
+                setEmail(fetchedStaffs.payload.email);
+                setFirstName(fetchedStaffs.payload.first_name);
+                setPhoneNumber(fetchedStaffs.payload.phone_no);
+                setRole(fetchedStaffs.payload.role);
+            } else {
+                console.warn("No staff data found.");
+            }
+        } catch (error) {
+            console.error("Error fetching fields data:",error);
+        }
+    };
+
     return (
         <div>
             <style>{'body { overflow-y: scroll; }'}</style>
@@ -42,6 +82,7 @@ export const Vehicle = () => {
                         <input id="txtRemarks" className="form-control" type="text" aria-label="default input example"/>
                     </div>
 
+                    {/*Status*/}
                     <div id="vehicle-status-div">
                         <label htmlFor="txtStatus" id="lblStatus">Status (Available/Not Available) :</label>
                         <select className="form-select" id="txtStatus" required>
@@ -50,41 +91,60 @@ export const Vehicle = () => {
                         </select>
                     </div>
 
+                    {/*Search Staff*/}
                     <div id="search-employs-div">
                         <label id="lblSearchEmploys" htmlFor="txtSearchEmploys">Search Employs :</label>
                         <input id="txtSearchEmploys" className="form-control" type="text"
-                               placeholder="Enter employee name or ID" aria-label="default input example"/>
-                        <button id="btnSearchEmploys" type="button" className="btn btn-primary">Search</button>
+                               placeholder="Enter employee name or ID"
+                               aria-label="default input example"
+                               value={SearchedStaff}
+                               onChange={(e) => setSearchedStaff(e.target.value)}/>
+                        <button id="btnSearchEmploys" type="button" className="btn btn-primary" onClick={handleStaffSearch}>Search</button>
                     </div>
 
+                    {/*Staff Email*/}
                     <div id="employee-email-vehicle-div">
                         <label id="lblVehicleEmail" htmlFor="txtVehicleEmail">Email :</label>
                         <input id="txtVehicleEmail" className="form-control" type="email"
-                               aria-label="default input example"/>
+                               aria-label="default input example"
+                               value={email}
+                               onChange={(e) => setEmail(e.target.value)}/>
                     </div>
 
+                    {/*Staff ID*/}
                     <div id="employee-member-id-vehicle-div">
                         <label id="lblVehicleMemberID" htmlFor="txtVehicleMemberID">Member ID :</label>
                         <input id="txtVehicleMemberID" className="form-control" type="text"
-                               aria-label="default input example"/>
+                               aria-label="default input example"
+                               value={staffID}
+                               onChange={(e) => setStaffID(e.target.value)}/>
                     </div>
 
+                    {/*Staff First Name*/}
                     <div id="employee-first-name-vehicle-div">
                         <label id="lblVehicleFirstName" htmlFor="txtVehicleFirstName">First Name :</label>
                         <input id="txtVehicleFirstName" className="form-control" type="text"
-                               aria-label="default input example"/>
+                               aria-label="default input example"
+                               value={firstName}
+                               onChange={(e) => setFirstName(e.target.value)}/>
                     </div>
 
+                    {/*Role*/}
                     <div id="employee-role-vehicle-div">
                         <label id="lblVehicleRole" htmlFor="txtVehicleRole">Role :</label>
                         <input id="txtVehicleRole" className="form-control" type="text"
-                               aria-label="default input example"/>
+                               aria-label="default input example"
+                               value={role}
+                               onChange={(e) => setRole(e.target.value)}/>
                     </div>
 
+                    {/*Phone Number*/}
                     <div id="employee-phone-number-vehicle-div">
                         <label id="lblVehiclePhoneNumber" htmlFor="txtVehiclePhoneNumber">Phone Number :</label>
                         <input id="txtVehiclePhoneNumber" className="form-control" type="text"
-                               aria-label="default input example"/>
+                               aria-label="default input example"
+                               value={phoneNumber}
+                               onChange={(e) => setPhoneNumber(e.target.value)}/>
                     </div>
 
                     {/*Buttons*/}
